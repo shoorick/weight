9e16019b15590243dc5ae46262d6c41db276aa10
Alexander Sapozhnikov <shoorick@mail.ru>
2020-10-09 15:17:27 +0500

    Add schema of database and initialization script for it

---
+++ db/init.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+connection = sqlite3.connect('database.db')
+
+with open('schema.sql') as file:
+    connection.executescript(file.read())
+
+cur = connection.cursor()
+
+cur.execute("INSERT INTO categories (id, title) VALUES (?, ?)",
+            (1, 'Weight')
+            )
+
+cur.execute("INSERT INTO entries (category_id, entry) VALUES (?, ?)",
+            (1, 88)
+            )
+
+connection.commit()
+connection.close()
