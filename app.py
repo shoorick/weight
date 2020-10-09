00c16faf406a4473bf102379ac86dbb26b3b3dc6
Alexander Sapozhnikov <shoorick@mail.ru>
2020-10-09 15:42:06 +0500

    Process GET /table — print collected data as table

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask, render_template
+import sqlite3
+
+def db_connect():
+    connection = sqlite3.connect('db/database.db')
+    connection.row_factory = sqlite3.Row
+    return connection
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/table')
+def table():
+    connection = db_connect()
+    entries = connection.execute('SELECT * FROM entries').fetchall()
+    connection.close()
+    return render_template('table.html', entries=entries)
