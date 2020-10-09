94e71e27cfbfadc73240b5cb8945e0af55009f01
Alexander Sapozhnikov <shoorick@mail.ru>
2020-10-09 16:12:39 +0500

    Add category output

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template
 import sqlite3
+from werkzeug.exceptions import abort
 
 def db_connect():
     connection = sqlite3.connect('db/database.db')
     connection.row_factory = sqlite3.Row
     return connection
+
+def get_category(id):
+    connection = db_connect()
+    category = connection.execute(
+        'SELECT * FROM categories WHERE id = ?',
+        (id,)
+    ).fetchone()
+    connection.close()
+    if category is None:
+        abort(404)
+    return category
 
 app = Flask(__name__)
 
@@ -18,3 +30,8 @@ def table():
     entries = connection.execute('SELECT * FROM entries').fetchall()
     connection.close()
     return render_template('table.html', entries=entries)
+
+@app.route('/categories/<int:id>')
+def category(id):
+    category = get_category(id)
+    return render_template('category.html', category=category)
